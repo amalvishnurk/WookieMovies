638f1a9ba098fdafa0e86f179cb9c56c454d142f
amalvishnurk <amalvishnu002@gmail.com>
2024-04-25 14:11:35 +0530

    final commit

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ const app = express()
 const mongoose = require("mongoose")
 const { DB } = require('./variables')
 const { PORT } = require('./variables')
-const fs = require('fs');
-const { Movie } = require('./models/movies')
 const bodyParser = require('body-parser');
 const api= require("./api")
+const cors= require('cors')
 
 app.listen(PORT, () => {
     console.log(`connected at ${PORT}`);
 })
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: ['http://localhost:3000']
+}))
 app.use("/api", api)
+
 
 
 
 mongoose.connect(DB, { autoCreate: true })
     .then(() => {
         console.log("Connected to Database!");
-        // importData()
     })
     .catch((error) => {
         console.log("Not connected to Database!");
